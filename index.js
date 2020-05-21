33542cb51d4e8e2f97c3b5387f8ad0955cf3b5de
albs-br <albs_br@yahoo.com.br>
2020-05-21 19:25:28 -0300

    1 file modified

--- index.js
+++ index.js
@@ -5,31 +5,40 @@ const SCREEN_HEIGHT = 480;
 
 
 const canvas = document.getElementsByTagName('canvas')[0];
-
-var ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 
 ClearScreen();
 
 
-var putPixel = (x, y) => {
+let putPixel = (x, y) => {
   ctx.fillRect(x, y, 1, 1);
 };
 
 
 let x = 0, y = SCREEN_HEIGHT / 2;
+let xPrev = x, yPrev= y;
 window.setInterval(function () {
   //for(let x=0; x<SCREEN_WIDTH; x++) {
   //}
   y += Math.floor((Math.random() * 11) + 1) - 6;
   putPixel(x, y);
+
+  // ctx.moveTo(xPrev, yPrev);
+  // ctx.lineTo(x, y);
+  // ctx.stroke(); 
+
+  xPrev = x;
+  yPrev= y;
+
   x++;
   if(x == SCREEN_WIDTH) { 
     x = 0;
     y = SCREEN_HEIGHT / 2;
     ClearScreen();
   }
-}, 1);
+
+}, 10);
 
 function ClearScreen() {
   ctx.fillStyle = "#000000"; 
